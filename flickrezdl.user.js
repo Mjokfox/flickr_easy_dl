3e058937272d6b3741f5b5b11f474f43e1513d23
mjokfox <66436642+Mjokfox@users.noreply.github.com>
2024-09-19 19:01:53 +0200

    add non www flickr match

--- flickrezdl.user.js
+++ flickrezdl.user.js
@@ -6,6 +6,7 @@
 // @updateURL    https://github.com/Mjokfox/flickr_easy_dl/raw/main/flickrezdl.user.js
 // @license      GPLv3
 // @match        https://www.flickr.com/*
+// @match        https://flickr.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=flickr.com
 // @grant        none
 // @inject-into  content
